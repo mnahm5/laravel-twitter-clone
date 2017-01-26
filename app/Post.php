af4c354eecac112af519cd5e91b138d3bff2f474
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-26 23:22:29 +1100

    relations between posts and users

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['body'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
